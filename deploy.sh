ff14aa6862ac795417827068eba16efc3ff95997
LeonLewiz <vostro501@gmail.com>
2026-08-25 14:54:21 +0000

    feat: complete day 3 pipeline with nginx, fastapi and postgresql

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+cd /var/www/html/my-nginx-site
+
+git reset --hard origin/main
+git pull origin main
+
+docker compose build --no-cache
+docker compose up -d --build --force-recreate
+
+echo " Полный деплой (Nginx + Python + Postgres) завершен!"
