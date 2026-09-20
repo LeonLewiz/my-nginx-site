ca9fad1b9cd85dc2ece8a6f634f01ac8555ab413
LeonLewiz <vostro501@gmail.com>
2026-09-20 17:23:40 +0000

    Added backups

---
+++ backup/backup.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+set -e
+
+STAMP=$(date +%Y-%m-%d-%H%M)
+FILE="/tmp/db-$STAMP.sql.gz"
+
+pg_dump -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" | gzip > "$FILE"
+
+aws --endpoint-url=https://storage.yandexcloud.net \
+  s3 cp "$FILE" "s3://devops-study-backops/db-$STAMP.sql.gz"
+
+rm -f "$FILE"
+echo "backup done: db-$STAMP.sql.gz"
